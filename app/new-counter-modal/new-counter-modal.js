916fa57a0bb712de9e210ecad46f725c31f0e2b7
Tazaf <obersonmathias@gmail.com>
2017-04-16 12:48:53 +0200

    Select the player with keyboard strokes

--- app/new-counter-modal/new-counter-modal.js
+++ app/new-counter-modal/new-counter-modal.js
@@ -1,37 +1,55 @@
-const electron = require('electron')
+const ipc = require('electron').ipcRenderer
 const path = require('path')
-const ipc = electron.ipcRenderer
-const BrowserWindows = electron.remote.BrowserWindow
 const events = require(path.join(__dirname, '..', 'lib', 'event-service.js'))
-console.log(events)
 
-const selfWin = BrowserWindows.getFocusedWindow()
-const choiceList = $('#choice-list')
-const okBtn = $('#ok')
+const $choiceList = $('#choice-list')
+const $okBtn = $('#ok')
 
-var nbPlayers;
+let nbPlayers
+let keyBuffer
+let keyBufferValue = ''
+
+$(document).keypress(startKeyBuffer)
 
 $('#cancel').click(() => ipc.send(events.nbPlayerModalClose))
 
 $('#ok').click(() => ipc.send(events.nbPlayerSelected, nbPlayers))
 
-$('li', choiceList).click(selectChoice)
+$('li', $choiceList).click(selectChoice)
 
-$("li", choiceList).hover(addSurrounders, removeSurrounders)
+$("li", $choiceList).hover(addSurrounders, removeSurrounders)
 
 function selectChoice() {
   nbPlayers = $(this).text()
-  console.log(nbPlayers)
-  $('li.active', choiceList).removeClass('active')
+  $('li.active', $choiceList).removeClass('active')
   $(this).addClass('active')
-  okBtn.hasClass('disabled') && okBtn.removeClass('disabled')
+  $okBtn.hasClass('disabled') && $okBtn.removeClass('disabled')
 }
 
 function removeSurrounders() {
-  $("li.surrounder", choiceList).removeClass('surrounder')
+  $("li.surrounder", $choiceList).removeClass('surrounder')
 }
 
 function addSurrounders() {
   $(this).prev().addClass('surrounder')
   $(this).next().addClass('surrounder')
+}
+
+function selectNbPlayerWithKeyboard() {
+  const value = parseInt(keyBufferValue)
+  console.log(value)
+  if (!isNaN(value) && value > 0 && value <= 10) {
+    $("li.active", $choiceList).removeClass('active')
+    $(`#${value}`, $choiceList).click()
+  }
+  keyBufferValue = ''
+}
+
+function startKeyBuffer(event) {
+  if ((event.which >= 48 && event.which <= 57) || (event.which >= 96 && event.which <= 105)) {
+    keyBufferValue += String.fromCharCode(event.keyCode)
+    console.log(keyBufferValue)
+    keyBuffer && clearTimeout(keyBuffer)
+    keyBuffer = setTimeout(selectNbPlayerWithKeyboard, 250)
+  }
 }
