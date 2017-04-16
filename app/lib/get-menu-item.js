5067f5f36fe8a7ca34cb1d9d8164f1d2a7f02216
Tazaf <obersonmathias@gmail.com>
2017-04-16 12:47:59 +0200

    Updated the get-menu-service

--- app/lib/get-menu-item.js
+++ app/lib/get-menu-item.js
@@ -1,27 +1,53 @@
-module.exports = function getItem(id, menu) {
+exports.byId = getItemById
+exports.byLabel = getItemByLabel
+
+/**
+ * Gets a specific Menu Item from the specified Menu that matches the provided label.
+ * @param {*} label The label of the Menu Item to retrieve
+ * @param {*} menu The Menu from which the Menu Item is to be retrieved
+ */
+function getItemByLabel(label, menu) {
+
+  return getItem(menu, matchLabel)
+
+  function matchLabel(item) {
+    return item.label.toString().toLowerCase() === label.toString().toLowerCase()
+  }
+}
+
+/**
+ * Gets a specific Menu Item from the specified Menu that matches the provided id.
+ * @param {*} id The label of the Menu Item to retrieve
+ * @param {*} menu The Menu from which the Menu Item is to be retrieved
+ */
+function getItemById(id, menu) {
+
+  return getItem(menu, matchId)
+
+  function matchId(item) {
+    return item.id === id
+  }
+}
+
+/**
+ * 
+ * @param {*} menu The Menu from which the Menu Item should be retrieved
+ * @param {*} matchFunction A filter function that takes each item and returns it only if it matches a specific criteria
+ */
+function getItem(menu, matchFunction) {
   for (i in menu.items) {
     if (menu.items[i].submenu && menu.items[i].submenu.items) {
-      var subItem = menu.items[i].submenu.items.find((item) => {
-        return item.id === id
-      })
-
+      var subItem = menu.items[i].submenu.items.find(matchFunction)
       if (subItem) {
         return subItem
       } else {
-        var menuItem = menu.items.find((item) => {
-          return item.id === id
-        })
-
+        var menuItem = menu.items.find(matchFunction)
         if (menuItem) {
           return menuItem
         }
       }
-
     } else {
-      var menuItem = menu.items.find((item) => {
-        return item.id === id
-      })
-
+      var menuItem = menu.items.find(matchFunction)
       if (menuItem) {
         return menuItem
       }
