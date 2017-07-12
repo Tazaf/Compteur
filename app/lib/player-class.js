bdc1cbed1012c89dadde410fe5d2cfe1d07a63d6
Mathias Oberson <Tazaf@users.noreply.github.com>
2017-07-12 10:20:38 +0200

    Update player-class.js

--- app/lib/player-class.js
+++ app/lib/player-class.js
@@ -3,7 +3,7 @@ const Logger = require(path.join(__dirname, 'logger.js'))
 const Settings = require(path.join(__dirname, 'settings-constants.js'))
 
 function Player(values) {
-  if (!values.hasOwnProperty('id')) throw new SyntaxError(`The Player constructor expecte at least an 'id' property on its values param object.`)
+  if (!values.hasOwnProperty('id')) throw new SyntaxError(`The Player constructor expects at least an 'id' property on its values param object.`)
   this.id = values.id
   this.name = values.name || null
   this.score = values.score || 0
